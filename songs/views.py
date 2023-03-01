36b199874ac38b3546ac7f940b4a5a00bea7ae4e
DeVante Parker <Devante.parker15@gmail.com>
2023-02-28 23:20:26 -0600

    added songs as a user, updated my admin in my songs app along with my views.py.

--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Song
+from .serializers import SongSerializer
 # Create your views here.
 @api_view(['GET', 'POST'])
 def song_list(request):
 
-    return Response('Ok')
+    songs = Song.objects.all()
+
+    serializer = SongSerializer(songs, many=True)
+
+    return Response(serializer.data)
 
         
